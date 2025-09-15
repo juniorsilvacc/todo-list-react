032265387aae599ac50cf38f6abc7ce16a6588c2
juniorsilvaneg <juniorsilvacc@hotmail.com>
2025-09-15 18:49:42 -0300

    criando component Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,22 @@
+import type { ChangeEvent } from "react";
+import styles from "./Button.module.css";
+import AddIcon from "../../assets/add.svg?react";
+
+type Props = {
+    value: string;
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    placeholder: string;
+}
+ 
+
+export const Input = ({ value, onChange, placeholder }: Props) => {
+    return (
+        <div className={styles.container}>
+            <input
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+            />
+        </div>
+    )
+}
