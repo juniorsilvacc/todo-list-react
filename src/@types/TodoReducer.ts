f75d2b8b64ce377da6239f5cbc70f08536d6dd73
juniorsilvaneg <juniorsilvacc@hotmail.com>
2025-09-15 14:57:17 -0300

    criando os reduces

--- src/@types/TodoReducer.ts
+++ src/@types/TodoReducer.ts
@@ -2,23 +2,21 @@ import type { Todo } from "./Todo";
 
 export type TodoState = Todo[]
 
-type TodoWithOptionalProps = {
-    [Key in keyof Todo]?: Todo[Key]
-}
+type TodoWithOptionalProps = Partial<Todo>
 
 type AddTodo = {
     type: 'ADD',
     payload: Todo | Todo[]
 }
 
-type ChangeTodo = [
+type ChangeTodo = {
     type: 'CHANGE',
     payload: TodoWithOptionalProps & { index: number }
-]
+}
 
-type DeleteTodo = [
+type DeleteTodo = {
     type: 'DELETE',
     payload: { index: number }
-]
+}
 
 export type TodoActions = AddTodo | ChangeTodo | DeleteTodo
---
+++ src/contexts/reducers/todoReducer.ts
@@ -0,0 +1,24 @@
+import type { TodoActions, TodoState } from "../../@types/TodoReducer";
+
+export const initialState: TodoState = []
+
+export const todoReducer = (state: TodoState, action: TodoActions): TodoState => {
+    switch(action.type) {
+        case 'ADD':
+            const { payload } = action
+            return !Array.isArray(payload) ? [...state, payload] : payload
+        case 'CHANGE':
+            const changedTodo = state.map((item, key) => {
+                if (key == action.payload.index) {
+                    item.title = action.payload.title ?? item.title
+                    item.isDone = action.payload.isDone || item.isDone
+                }
+                return item
+            })
+            return changedTodo
+        case 'DELETE':
+            return state.filter((_item, key) => key != action.payload.index)
+        default:
+            return state
+  }
+}
